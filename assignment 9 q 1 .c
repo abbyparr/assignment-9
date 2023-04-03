9688128d345ce60ec16bb3f440885fe7ef177720
abbyparr <123125559+abbyparr@users.noreply.github.com>
2023-04-03 18:38:51 -0400

    Add files via upload

---
+++ assignment 9 q 1 .c
@@ -0,0 +1,27 @@
+numbers = input('input 9 numbers')
+if len(numbers == 9):
+    grid = []
+    numbers = grid
+    
+for i in numbers :
+    grid.append(i)
+    if(i % numbers):
+        grid.append(number.copy())
+        numbers.clear()
+return grid
+    
+else:
+if (numbers != [0:2])
+print('item does not have 3 elements')
+
+if (input != 9)
+print('incorrect: input another input with correct numbers')
+
+if(grid != numbers)
+print('grid is not legal. Some values may be illegal, as they are not numbers')
+
+x = [number for numbers if numbers.count(number) > 1]
+duplicates = list(set(x))
+
+else(numbers = duplicates)
+print('grid is not legal. It may contain duplicate numbers')
